adba4d73159594a43421a624d5d92013e4d97522
Souvik_Mondal <sm6984767@gmail.com>
2025-01-29 14:44:33 +0530

    added reggisyter

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 
 const AppRoutes = () => (
-  <Router>
+  // <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
-  </Router>
+  // </Router>
 );
 
 export default AppRoutes;
